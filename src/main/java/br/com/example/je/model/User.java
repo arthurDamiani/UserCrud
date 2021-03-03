02f1731c6cb346ae08467209438f22be8fb6e4d4
unknown <arthur.damiani@gmail.com>
2021-03-03 11:41:02 -0300

    email unique

--- src/main/java/br/com/example/je/model/User.java
+++ src/main/java/br/com/example/je/model/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -22,6 +23,7 @@ public class User implements UserDetails {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	@Column(unique = true)
 	private String email;
 	private String password;
 	@ManyToMany(fetch = FetchType.EAGER)
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,5 @@
 INSERT INTO USER(name, email, password) VALUES('Damiani', 'arthur.damiani@gmail.com', '$2a$10$RI.4J4NiCvSZ8connjflzubl9K9pjZHukRYB9RxXMgR5nMO9Cg8TC');
-INSERT INTO USER(name, email, password) VALUES('Jéssica', 'jessicadesaxavier18@gmail.com', '$2a$10$RI.4J4NiCvSZ8connjflzubl9K9pjZHukRYB9RxXMgR5nMO9Cg8TC');
 
 INSERT INTO PROFILE(id, name) VALUES(1, 'ROLE_MODERADOR');
-INSERT INTO PROFILE(id, name) VALUES(2, 'ROLE_ALUNO');
 
-INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(1, 1);
-INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(2, 2);
+INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(1,1);
