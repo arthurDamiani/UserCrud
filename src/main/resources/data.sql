cf527417cd00b18486dcd9a6b906d09e653607fa
arthurDamiani <71735476+arthurDamiani@users.noreply.github.com>
2021-03-30 19:52:28 -0300

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,8 @@
 INSERT INTO USER(name, email, password) VALUES('Damiani', 'arthur.damiani@gmail.com', '$2a$10$RI.4J4NiCvSZ8connjflzubl9K9pjZHukRYB9RxXMgR5nMO9Cg8TC');
+INSERT INTO USER(name, email, password) VALUES('ADM', 'adm@gmail.com', '$2a$10$RI.4J4NiCvSZ8connjflzubl9K9pjZHukRYB9RxXMgR5nMO9Cg8TC');
 
 INSERT INTO PROFILE(id, name) VALUES(1, 'ROLE_STUDENT');
 INSERT INTO PROFILE(id, name) VALUES(2, 'ROLE_ADM');
 
 INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(1,2);
+INSERT INTO USER_PROFILES(user_id, profiles_id) VALUES(2,2);
